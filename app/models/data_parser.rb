0b20b6670aace67185313387eafa5c37c4df4248
Dan Arrington <dan.arrington@gmail.com>
2014-04-29 07:27:52 -0700

    Parse and save unknown courses

--- app/models/data_parser.rb
+++ app/models/data_parser.rb
@@ -1,21 +1,17 @@
 class DataParser
 
   def get_latest_times
+    @known_course_ids = Course.ids
     doc = get_doc_to_parse
     doc.css('.cubeWrapper').each { |cube| save_time parse_time_cube cube }
   end
 
   def parse_time_cube(cube)
-    course_id = cube.css('.courseName a').first['productid']
-    save_course_info get_course_info course_id
-  end
-
-  def get_course_info(course_id)
-    doc = get_course_doc course_id
-  end
-
-  def get_course_doc(course_id)
-    Nokogiri::HTML(open("http://www.golfnow.com/CoursePopUp.aspx?facilityid=#{course_id}&dl=false"))
+    course_id = cube.css('.courseName a').first['productid'].to_i
+    unless @known_course_ids.include?(course_id)
+      course = CourseParser.new.get_course_info(course_id)
+      course.save!
+    end
   end
 
   def save_time(tee_time)
@@ -23,6 +19,7 @@ class DataParser
   end
 
   def get_doc_to_parse
+    url = "http://www.golfnow.com/seattle/tee-times/golf-courses/wa---seattle-metro/search?FID=-1&OID=9543&AID=61&SID={01E99287-F7C4-4A11-9828-3A0C5F9499B0}&FDT=3/27/2014%2012:00:00%20AM&TDT=3/27/2014%2012:00:00%20AM&PPN=all"
     doc = Nokogiri::HTML(open('http://www.golfnow.com/something'))
   end
 end
--- spec/models/data_parser_spec.rb
+++ spec/models/data_parser_spec.rb
@@ -5,12 +5,39 @@ describe DataParser do
   subject(:data_parser) {DataParser.new}
   context 'parsing page with unknown course' do
 
+    before :each do
+      f = File.open('spec/test_pages/one_result.html')
+      doc = Nokogiri::XML(f)
+      f.close
+      subject.stub(:get_doc_to_parse){doc}
+
+      f2 = File.open('spec/test_pages/kenwanda.aspx.html')
+      doc2 = Nokogiri::XML(f2)
+      f2.close
+      CourseParser.any_instance.stub(:get_course_doc){doc2}
+    end
+
     it 'saves the unknown course' do
 
-      pending 'save course info first'
       subject.get_latest_times
 
       expect(Course.count).to eq 1
+    end
+  end
+
+  context 'parsing page with only known courses' do
+    before :each do
+      f = File.open('spec/test_pages/one_result.html')
+      doc = Nokogiri::XML(f)
+      f.close
+      subject.stub(:get_doc_to_parse){doc}
+      FactoryGirl.create(:course, id: 1037905) #course id in one_result
+    end
+
+    it 'does not save any courses' do
+      CourseParser.should_receive(:new).exactly(0).times
+
+      subject.get_latest_times
     end
   end
 end
